1e7f3e46b812cb53112a053f19f75928807a2ae0
Tom Overland <tomoverland03@gmail.com>
2020-08-11 11:03:38 -0500

    got the google maps to work!

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 
 // -------------------Google Maps------------------
-//defining map variable in global scope
-var map;
 //initialize and add the map
+let map;
+
 function initMap() {
-    //the location we are seeking
-    var locationName = {lat: -25.344, lng: 131.036};
-    //the map, centered at the location defined in above variable
-    map = new google.maps.Map($('#map'), {zoom: 4, center locationName});
-    //the marker, positioned at the location defined on line 6
-    var marker = new google.maps.Marker({position: locationName, map: map});
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
 }
+
 // -------------------National Park Service API ----------------
 
+let parksAPI = Quk7HRe6sAdTvcwxZOA6wxEoqK4orHEXoYBKts9n;
