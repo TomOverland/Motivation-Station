aeb90012d7371c65f746fde5e4b6edec7b37554f
Tom Overland <tomoverland03@gmail.com>
2020-08-11 20:11:27 -0500

    changed project idea, removing old code

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-
 // ------------------- Google Maps ------------------
 //use mapLat & mapLng to define the search locations from the national park response
 let mapLat;
@@ -11,40 +10,47 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     //change lat & lng - same note as line 5
     //used placeholder lat lng from yellowstone ntl park
-    center: { lat: 44.4280, lng: -110.5885 },
-    zoom: 8
+    center: { lat: 44.428, lng: -110.5885 },
+    zoom: 8,
   });
 }
 
 // ------------------- National Park Service API ----------------
-const stateArr = "CA";
+let stateArr = $("#city-search").val();
 
-const parksAPI = 'Quk7HRe6sAdTvcwxZOA6wxEoqK4orHEXoYBKts9n';
-let parksQueryURL = 'https://api.nps.gov/api/v1/parks?stateCode=' + stateArr + '&api_key=' + parksAPI;
-// defining stateArr in global scope as an empty string, 
+// function findStateArr() {
+//   stateArr = $("#city-search").val();
+// }
+
+const parksAPI = "Quk7HRe6sAdTvcwxZOA6wxEoqK4orHEXoYBKts9n";
+let parksQueryURL =
+  "https://api.nps.gov/api/v1/parks?stateCode=" +
+  stateArr +
+  "&api_key=" +
+  parksAPI;
+// defining stateArr in global scope as an empty string,
 // as it will later be reassigned within the event delegation function
 // and used in the .ajax query
 
-
-//ajax call to obtain national park data
-$.ajax({
+//--------------------- event delegation --------------------------
+$("#search-btn").on("click", function () {
+  //stops form from "submitting"
+  event.preventDefault();
+  //   stateArr = $("#city-search").val();
+  //   findStateArr();
+  //ajax call to obtain national park data
+  $.ajax({
     url: parksQueryURL,
     method: "GET",
   }).then(function (response) {
     console.log(response);
     console.log(parksQueryURL);
-    let results = response.list
+    let results = response.list;
+    let stateArr = "CA";
     //each new search will clear the previous, we will need to assign a current id
     // $("#current").empty();
-  
+  });
 });
 
-//--------------------- event delegation --------------------------
-$('search-btn').on('click', function() {
-    //stops form from "submitting"
-    event.preventDefault();
-    stateArr = $('city-search').text.val();
-    //test console.log
-    console.log(stateArr);
-        
-})
+//TO DOS:
+//1. obtain responses from
