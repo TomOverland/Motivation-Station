7ccb6d752a1cf2d695b3122842e556c16beedf92
Tom Overland <tomoverland03@gmail.com>
2020-08-12 11:23:22 -0500

    added comments to js

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,7 @@
 // -------- Random Quotes from QuoteGarden API ---------
 let quoteGardenURL = "https://quote-garden.herokuapp.com/api/v2/quotes/random";
-//ajax call for current weather stats for a city
-// $.ajax({
-//   url: quoteGardenURL,
-//   method: "GET",
-// }).then(function (response) {
-//   console.log(response);
-//   console.log(quoteGardenURL);
-//   //each new search will clear the previous
-//   //$("#current").empty();
-//   let quoteAuthor = $("<h4>").text(response.quote.quoteAuthor);
-//   let quoteText = $("<h6>").text(response.quote.quoteText);
-//   let newDiv = $("<div>");
 
-//   newDiv.append(quoteText, quoteAuthor);
-//   $("#quote").html(newDiv);
-// });
+// -------- Event Delegation -------
 
 $("#btn").on("click", function (event) {
   $.ajax({
