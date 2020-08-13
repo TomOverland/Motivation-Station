4fac9a933dd11b8f56ea84b1c38acfbd949d9ec7
Tom Overland <tomoverland03@gmail.com>
2020-08-13 17:20:25 -0500

    adding something

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,6 @@ $.ajax({
   $("#harvard-img").html("<img src=" + harvardImg + "></img>");
 });
 
-
 // -------- Event Delegation -------
 $("#btn").on("click", function (event) {
   event.preventDefault();
@@ -32,21 +31,19 @@ $("#btn").on("click", function (event) {
     let quoteAuthor = response.quote.quoteAuthor;
     let quoteAuthorEl = $("<h6 id='author'>").text(quoteAuthor);
     let quoteText = response.quote.quoteText;
-    let quoteTextEl =$("<h4>").text(quoteText);
+    let quoteTextEl = $("<h4>").text(quoteText);
     let newDiv = $("<div>");
     newDiv.append(quoteTextEl, quoteAuthorEl);
     $("#quote").html(newDiv);
     //save each quote to local storage, and call the "get" function when the save btn is
     //pressed, causing the saved item to be created on the saved page
     let savedQuoteArr = {
-      quotetext: quoteText, 
-      author: quoteAuthor
+      quotetext: quoteText,
+      author: quoteAuthor,
     };
     localStorage.setItem("savedQuote", JSON.stringify(savedQuoteArr));
   });
 });
-
-
 
 //This doesn't currently save.  Needs to be fixed.
 $("#save-btn").on("click", function (event) {
@@ -57,14 +54,12 @@ $("#save-btn").on("click", function (event) {
     listQuoteEl = $("<p>").text(listQuote);
     listAuthor = getSavedArr.author;
     listAuthorEl = $("<p>").text(listAuthor);
-    newListItem = $("<li>")
-    newListItem.append(listQuote, listAuthor)
+    newListItem = $("<li>");
+    newListItem.append(listQuote, listAuthor);
     $("#list").html(newListItem);
-  };
+  }
   saveQuotes();
 });
-
-
 
 //Add items to list
 //list.innerHTML += '<li>' + variable.value +'<li>';
@@ -72,5 +67,5 @@ $("#save-btn").on("click", function (event) {
 //clear button
 //$('#reset-btn').on("click", function(event) {
 //   event.preventDefault();
-//   ListeningStateChangedEvent.value = '';
+//   list.value = '';
 // })
